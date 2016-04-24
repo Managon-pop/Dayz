003ff0d6fac6c3aa5326b5bdc75169b32a7a209c
Manago-n! <github123pop@gmail.com>
2016-04-24 22:36:51 +0900

    Update ReloadTask.php

--- src/Managon/tasks/ReloadTask.php
+++ src/Managon/tasks/ReloadTask.php
@@ -1,11 +1,19 @@
 <?php
 
 use pocketmine\scheduler\PluginTask;
+use Managon\Dayz;
 
 class ReloadTask extends PluginTask{
 
-  public function __construct(PluginBase $main){
+  public function __construct(PluginBase $main,$name){
       parent::__construct($main);
       $this->main = $main;
+      $this->name = $name;
+  }
+  
+  public function onRun($tick){
+    $dayz = new Dayz;
+    $gun = $dayz->getGun($this->name);
+    $gun->reload();
   }
 }
